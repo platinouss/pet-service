83c71f7699e3ccd9d232822c0f2aa3fa561384f8
platinouss <platinouss@gmail.com>
2025-02-12 15:07:50 +0900

    Init: 프로젝트 초기화 설정

---
+++ src/test/java/com/petsitter/kt/PetSitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petsitter.kt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetSitterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
